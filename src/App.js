4173fc97b40e6083d1d9823e634640ead1151693
eusbeidy <eurimary@gmail.com>
2023-03-01 14:31:19 -0500

    practicando apis

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Cards } from './components/Cards';
+
+function App() {
+
+
+
+  return (
+    <>
+    < Cards/>
+
+    </>
+  );
+}
+
+export default App;
